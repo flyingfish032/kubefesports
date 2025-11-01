0a926a44ee86978fa92a32a3dc48317c17c4ff00
flyingfish032 <hussaincme@gmail.com>
2025-11-01 10:20:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <Provider store={store}> */}
       <BrowserRouter>
         <App />
-      </BrowserRouter>
+      <BrowserRouter basename="/sportsl">
     {/* </Provider> */}
   </React.StrictMode>
 );
