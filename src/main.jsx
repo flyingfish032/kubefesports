5ae1c93a628b978f26de72db8982ac4147287eab
flyingfish032 <hussaincme@gmail.com>
2025-11-01 10:36:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-      <BrowserRouter>
-        <App />
       <BrowserRouter basename="/sportsl">
+        <App />
+      </BrowserRouter>
     {/* </Provider> */}
   </React.StrictMode>
 );
